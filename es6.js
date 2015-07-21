748de7872a6512b21a7e019dd5a331fd3dcb4e70
Trey Huffine <treyhuffine@gmail.com>
2015-07-20 17:30:09 -0700

    Convert all to ES6

--- es6.js
+++ es6.js
@@ -5,48 +5,27 @@ let myplace;
 let directionsService = new google.maps.DirectionsService();
 let currentStepIndex = 0, lastInfoWindow, instructions, position;
 
-import { interestingTypes, radiusInMeters } from './constants';
+import { interestingTypes as types, radiusInMeters as radius } from './constants';
 import ArrayWithRand from './arrayWithRandom';
-console.log(ArrayWithRand);
+import _ from 'lodash';
 
+let initialize = () => {
 
-function initialize() {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    myplace = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-
-    map = new google.maps.Map(document.getElementById('map-canvas'), { center: myplace, zoom: 17 });
-
-    var request = {
-      location: myplace,
-      radius: radiusInMeters,
-      types: interestingTypes
-    };
-
-    service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);
-  });
-
-  function callback(results, status) {
-    console.log(results);
+  let callback = (results, status) => {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
-      console.log(results);
-      // Array.prototype.sample = function() {
-      //   return this[Math.floor(Math.random()*results.length)];
-      // }
-      // let resultsWR = ArrayWithRand.from(results);
-      var randomPlace = ArrayWithRand.from(results).sample;
-      console.log(randomPlace);
+      // let randomPlace = ArrayWithRand.from(results).sample;
+      let randomPlace = _.sample(results);
       getDirections(randomPlace);
     }
-  }
+  };
 
-  function getDirections(place) {
-    var request = {
+  let getDirections = place => {
+    let request = {
       origin: myplace,
       destination: place.geometry.location,
       travelMode: google.maps.TravelMode.WALKING
     };
-    directionsService.route(request, function(result, status) {
+    directionsService.route(request, (result, status) => {
       if (status == google.maps.DirectionsStatus.OK) {
         destination = result.routes[0].legs[0];
         steps = result.routes[0].legs[0].steps;
@@ -54,9 +33,9 @@ function initialize() {
         document.getElementById("next").classList.remove("hidden");
       }
     });
-  }
+  };
 
-  function displayCurrentStep() {
+  let displayCurrentStep = () => {
     if (lastInfoWindow) {
       lastInfoWindow.close();
     }
@@ -66,32 +45,41 @@ function initialize() {
       document.getElementById("next").classList.add("hidden");
       position = destination.end_point;
     } else {
-      var currentStep = steps[currentStepIndex];
+      let currentStep = steps[currentStepIndex];
       position = currentStep.end_point;
       instructions = currentStep.instructions;
     }
 
-    var infowindow = new google.maps.InfoWindow({
-      content: instructions
-    });
+    let infowindow = new google.maps.InfoWindow({ content: instructions });
 
-    var marker = new google.maps.Marker({
-      position: position,
-      map: map,
+    let marker = new google.maps.Marker({
+      position,
+      map,
       animation: google.maps.Animation.DROP
     });
 
     infowindow.open(map, marker);
     lastInfoWindow = infowindow;
-  }
+  };
 
-  var nextButton = document.getElementById("next");
+  navigator.geolocation.getCurrentPosition(position => {
+    myplace = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    map = new google.maps.Map(document.getElementById('map-canvas'), { center: myplace, zoom: 17 });
+    let request = {
+      location: myplace,
+      radius,
+      types
+    };
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+  });
 
-  nextButton.addEventListener("click", function() {
+  let nextButton = document.getElementById("next");
+
+  nextButton.addEventListener("click", () =>{
     currentStepIndex++;
     displayCurrentStep();
   });
-
-}
+};
 
 google.maps.event.addDomListener(window, 'load', initialize);
